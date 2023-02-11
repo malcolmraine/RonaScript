c03b7e27bdbefa9066d932e8f00e5008351f673b
Malcolm Hall <malcolmrainehall@gmail.com>
2023-02-10 23:04:15 -0500

    Revert "Revert "Add more numerical expressions to functional test and fix code gen for unary expressions""
    This reverts commit 6fe1fd72b1611c9679b06a1cff61ace8bd05385d.

--- src/codegen/RnCodeGenVisitor.cpp
+++ src/codegen/RnCodeGenVisitor.cpp
@@ -397,19 +397,26 @@ InstructionBlock RnCodeGenVisitor::Visit(BoolLiteral* node) {
 
 /*****************************************************************************/
 InstructionBlock RnCodeGenVisitor::Visit(UnaryExpr* node) {
-    InstructionBlock instructions = GeneralVisit(node->expr);
+
     if (node->op == "++") {
-        instructions.push_back(new RnInstruction(OP_UNARY_INCREMENT));
+        return {new RnInstruction(
+            OP_UNARY_INCREMENT,
+            RnObject::InternValue(dynamic_pointer_cast<Name>(node->expr)->value))};
     } else if (node->op == "--") {
-        instructions.push_back(new RnInstruction(OP_UNARY_DECREMENT));
-    } else if (node->op == "-") {
-        instructions.push_back(new RnInstruction(OP_UNARY_NEGATION));
-    } else if (node->op == "~") {
-        instructions.push_back(new RnInstruction(OP_UNARY_INVERT));
-    } else if (node->op == "!") {
-        instructions.push_back(new RnInstruction(OP_UNARY_NOT));
+        return {new RnInstruction(
+            OP_UNARY_DECREMENT,
+            RnObject::InternValue(dynamic_pointer_cast<Name>(node->expr)->value))};
+    } else {
+        InstructionBlock instructions = GeneralVisit(node->expr);
+        if (node->op == "-") {
+            instructions.push_back(new RnInstruction(OP_UNARY_NEGATION));
+        } else if (node->op == "~") {
+            instructions.push_back(new RnInstruction(OP_UNARY_INVERT));
+        } else if (node->op == "!") {
+            instructions.push_back(new RnInstruction(OP_UNARY_NOT));
+        }
+        return instructions;
     }
-    return instructions;
 }
 
 /*****************************************************************************/
