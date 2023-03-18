77701bcb78d300d951e9df7dd06799c784791c84
Malcolm Hall <malcolmrainehall@gmail.com>
2023-03-18 10:50:26 -0400

    Include more diverse types and values in var declaration stress test

--- test/functional/var_decl_stress_test/generate_source.py
+++ test/functional/var_decl_stress_test/generate_source.py
@@ -1,6 +1,52 @@
+import random
+import string
 
 
-with open("source.rn", "w+") as f:
-    for n in range(0, 10000):
-        f.write(f"var test_{n}: int = 99;\n")
-    f.write("print(\"Finished\");")
+def make_rand_string():
+    return "\"" + "".join(random.choices(string.ascii_uppercase + string.digits, k=100)) + "\""
+
+
+def make_rand_float():
+    return random.uniform(-100000, 100000)
+
+
+def make_rand_int():
+    return random.randint(-100000, 100000)
+
+
+def make_rand_bool():
+    return random.choice(["true", "false"])
+
+
+type_generators = {
+    "float": make_rand_float,
+    "int": make_rand_int,
+    "string": make_rand_string,
+    "array": None,
+    "bool": make_rand_bool,
+}
+
+
+def make_rand_decl():
+    t = random.choice([key for key in type_generators.keys()])
+    v = type_generators[t]()
+    return t, v
+
+
+def make_rand_array():
+    result = []
+    for n in range(random.randint(0, 5)):
+        result.append(make_rand_float())
+        result.append(make_rand_bool())
+        result.append(make_rand_int())
+        result.append(make_rand_string())
+    return str(result)
+
+
+if __name__ == "__main__":
+    type_generators["array"] = make_rand_array
+    with open("source.rn", "w+") as f:
+        for n in range(0, 10000):
+            t, v = make_rand_decl()
+            f.write(f"var test_{n}: {t} = {v};\n")
+        f.write("print(\"Finished\");")
