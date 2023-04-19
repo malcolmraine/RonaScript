caea75f042e6c5cd87a7053107b21f5bbddcb703
Malcolm Hall <malcolmrainehall@gmail.com>
2023-04-19 14:30:41 -0400

    Add empty classes for binary reading and writing

---
+++ src/codegen/BinaryReader.cpp
@@ -0,0 +1,13 @@
+/*****************************************************************************
+* File: BinaryReader.cpp
+* Description:
+* Author: Malcolm Hall
+* Date: 4/19/23
+* Version: 1
+*
+*****************************************************************************/
+//
+// Created by Malcolm Hall on 4/19/23.
+//
+
+#include "BinaryReader.h"
