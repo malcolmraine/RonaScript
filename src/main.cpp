1173683f227fc6a61bf98c86e7855b392995f64f
Malcolm Hall <malcolmrainehall@gmail.com>
2023-04-22 07:48:34 -0400

    Update main functions to handle different compile/run modes

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <fstream>
 #include <iostream>
 #include <set>
+#include "codegen/RnBinary.h"
 #include "codegen/RnCodeGenerator.h"
 #include "common/RnInternment.h"
 #include "lexer/Lexer.h"
@@ -17,9 +18,106 @@
 // @formatter:off
 #include "RnBuildInfo.h"
 
+ArgParser arg_parser;
+
+void Compile(const std::filesystem::path& infile, RnCodeGenerator& code_generator) {
+    Lexer lexer;
+    try {
+        lexer.LoadFile(std::filesystem::absolute(infile));
+        lexer.ProcessTokens();
+    } catch (const std::exception& e) {
+        Log::ERROR("Syntax Error: " + std::string(e.what()));
+        return;
+    }
+
+    if (arg_parser.IsSet("-t")) {
+        for (auto& token : lexer.tokens) {
+            Log::INFO(token->ToString());
+        }
+    }
+
+    if (lexer.tokens.empty()) {
+        return;
+    }
+
+    Parser parser;
+    try {
+        parser.working_dir = infile.parent_path();
+        parser.file = infile;
+        parser.LoadTokens(lexer.tokens);
+        parser.Parse();
+
+        if (arg_parser.IsSet("-a")) {
+            Log::INFO(parser.DumpsAst());
+        }
+
+        if (!arg_parser.IsSet("--no-validation")) {
+            RnAstValidator validator;
+            validator.Visit(parser.ast->root.get());
+        }
+    } catch (const std::exception& e) {
+        Log::ERROR("Parse Error: " + std::string(e.what()));
+        return;
+    }
+
+    try {
+        code_generator.Generate(parser.ast);
+        if (arg_parser.IsSet("-p")) {
+            size_t index = 0;
+            for (auto& instruction : code_generator.GetInstructions()) {
+                Log::INFO(String::Pad(std::to_string(index++), 6) +
+                          instruction->ToString());
+            }
+        }
+    } catch (const std::exception& e) {
+        Log::ERROR("Codegen Error: " + std::string(e.what()));
+        return;
+    }
+}
+
+/*****************************************************************************/
+void Run(const InstructionBlock& instructions) {
+    if (instructions.empty()) {
+        return;
+    }
+    auto vm = RnVirtualMachine::GetInstance();
+    try {
+        vm->LoadInstructions(instructions);
+        RnIntNative exit_code = vm->Run();
+    } catch (const std::exception& e) {
+        Log::ERROR("Runtime Error: " + std::string(e.what()));
+        return;
+    }
+    delete vm;
+}
+
+/*****************************************************************************/
+void Write(const std::filesystem::path& outfile, const InstructionBlock& instructions) {
+    BinaryWriter writer(std::filesystem::absolute(outfile));
+    writer.SetInstructions(instructions);
+    writer.Write();
+}
+
+/*****************************************************************************/
+void Read(const std::filesystem::path& infile, InstructionBlock& instructions) {
+    BinaryReader reader(std::filesystem::absolute(infile));
+    reader.Read(instructions);
+}
+
+/*****************************************************************************/
+void PrintInstructions(const InstructionBlock& instructions) {
+    if (arg_parser.IsSet("-p")) {
+        size_t index = 0;
+        for (auto& instruction : instructions) {
+            Log::INFO(String::Pad(std::to_string(index++), 6) +
+                      instruction->ToString());
+        }
+    }
+}
+
 /*****************************************************************************/
 void RonaScriptMain(int argc, char* argv[]) {
-    ArgParser arg_parser;
+
     arg_parser.SetMainDescription("Usage: RonaScript <file> [options...]");
     arg_parser.AddArgument("<file>", {}, "Input file (*.rn | *.rnc)");
     arg_parser.AddArgument("-c", {}, "Compile to *.rnc file");
@@ -45,93 +143,33 @@ void RonaScriptMain(int argc, char* argv[]) {
     }
 
     std::filesystem::path const file = arg_parser.GetInputFile();
-    std::filesystem::path const cfile = file.string() + "c";
-
+    if (!std::filesystem::exists(file)) {
+        Log::ERROR("RonaScript: Error: Input file does not exist: " + file.string());
+        return;
+    }
     if (std::filesystem::file_size(file) == 0) {
         return;
     }
 
-    Lexer lexer;
-    try {
-        lexer.LoadFile(file);
-        lexer.ProcessTokens();
-    } catch (const std::exception& e) {
-        Log::ERROR("Syntax Error: " + std::string(e.what()));
+    InstructionBlock instructions;
+
+    if (file.extension() == ".rnc") {
+        Read(file, instructions);
+        PrintInstructions(instructions);
+        Run(instructions);
         return;
-    }
-
-    if (arg_parser.IsSet("-t")) {
-        for (auto& token : lexer.tokens) {
-            Log::INFO(token->ToString());
-        }
-    }
-
-    Parser parser;
-    try {
-        parser.working_dir = file.parent_path();
-        parser.file = file;
-        parser.LoadTokens(lexer.tokens);
-        parser.Parse();
-
-        if (arg_parser.IsSet("-a")) {
-            Log::INFO(parser.DumpsAst());
-        }
-
-        if (!arg_parser.IsSet("--no-validation")) {
-            RnAstValidator validator;
-            validator.Visit(parser.ast->root.get());
-        }
-    } catch (const std::exception& e) {
-        Log::ERROR("Parse Error: " + std::string(e.what()));
-        return;
-    }
-
-    RnCodeGenerator code_generator;
-    try {
-        code_generator.Generate(parser.ast);
-        if (arg_parser.IsSet("-p")) {
-            size_t index = 0;
-            for (auto& instruction : code_generator.GetInstructions()) {
-                Log::INFO(String::Pad(std::to_string(index++), 6) +
-                          instruction->ToString());
+    } else {
+        RnCodeGenerator code_generator;
+        if (arg_parser.IsSet("-c")) {
+            Compile(file, code_generator);
+            instructions = code_generator.GetInstructions();
+            Write(std::filesystem::path(file.string() + "c"), instructions);
+            if (!arg_parser.IsSet("-r")) {
+                Run(instructions);
             }
         }
-    } catch (const std::exception& e) {
-        Log::ERROR("Codegen Error: " + std::string(e.what()));
-        return;
+        PrintInstructions(instructions);
     }
-
-    std::ofstream ofs;
-    ofs.open(cfile.c_str(), std::ios::binary);
-    ofs.write("$$CONST$$", 9);
-    for (size_t i = 1; i <= RnConstStore::object_internment->GetIndex(); i++) {
-        auto object = RnConstStore::object_internment->GetInternedItem(i);
-        char* buf = new char[object->GetByteSize()];
-        object->GetBytes(buf);
-        ofs.write(buf, object->GetByteSize());
-        delete[] buf;
-    }
-
-    for (auto instruction : code_generator.GetInstructions()) {
-        char buf[4] = {};
-        buf[0] = static_cast<char>(instruction->GetOpcode());
-        buf[1] = static_cast<char>(instruction->GetArg1());
-        buf[2] = static_cast<char>(instruction->GetArg2());
-        buf[3] = static_cast<char>(instruction->GetArg3());
-        ofs.write(buf, 4);
-    }
-    ofs.close();
-
-    auto vm = RnVirtualMachine::GetInstance();
-    try {
-        vm->LoadInstructions(code_generator.GetInstructions());
-        RnIntNative exit_code = vm->Run();
-    } catch (const std::exception& e) {
-        Log::ERROR("Runtime Error: " + std::string(e.what()));
-        return;
-    }
-
-    delete vm;
 }
 
 /*****************************************************************************/
