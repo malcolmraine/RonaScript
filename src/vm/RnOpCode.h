afd61d9afdf9932b8693035b50ca7be5f335b034
Malcolm Hall <malcolmrainehall@gmail.com>
2023-11-26 18:21:02 -0500

    Organize opcodes a bit

--- src/vm/RnOpCode.h
+++ src/vm/RnOpCode.h
@@ -31,63 +31,63 @@
 #include "../common/RnType.h"
 
 typedef enum : int {
-    OP_LOAD_ATTR,
     OP_RESOLVE_NAMESPACE,
-    OP_BINARY_ADD,    // op1:          op2:
-    OP_BINARY_SUB,    // op1:          op2:
-    OP_BINARY_MUL,    // op1:          op2:
-    OP_BINARY_DIV,    // op1:          op2:
-    OP_BINARY_MOD,    // op1:          op2:
-    OP_BINARY_GTE,    // op1:          op2:
-    OP_BINARY_LTE,    // op1:          op2:
-    OP_BINARY_GT,     // op1:          op2:
-    OP_BINARY_LT,     // op1:          op2:
-    OP_BINARY_EQ,     // op1:          op2:
-    OP_BINARY_NEQ,    // op1:          op2:
-    OP_BINARY_POWER,  // op1:          op2:
-    OP_BINARY_RSH,    // op1:          op2:
-    OP_BINARY_LSH,    // op1:          op2:
-    OP_BINARY_OR,     // op1:          op2:
-    OP_BINARY_XOR,    // op1:          op2:
-    OP_BINARY_AND,    // op1:          op2:
+    OP_BINARY_ADD,    
+    OP_BINARY_SUB,    
+    OP_BINARY_MUL,    
+    OP_BINARY_DIV,    
+    OP_BINARY_MOD,    
+    OP_BINARY_GTE,    
+    OP_BINARY_LTE,    
+    OP_BINARY_GT,     
+    OP_BINARY_LT,     
+    OP_BINARY_EQ,     
+    OP_BINARY_NEQ,    
+    OP_BINARY_POWER,  
+    OP_BINARY_RSH,    
+    OP_BINARY_LSH,    
+    OP_BINARY_OR,     
+    OP_BINARY_XOR,    
+    OP_BINARY_AND,    
     OP_LOGICAL_OR,
     OP_LOGICAL_AND,
-    OP_STORE,      // op1: name     op2:
-    OP_POP,        // op1:          op2:
-    OP_UNARY_NOT,  // op1: name     op2:
+    OP_UNARY_NOT,  
     OP_TRY_CONTEXT,
-    OP_UNARY_INVERT,     // op1: name     op2:
-    OP_UNARY_DECREMENT,  // op1: name     op2:
-    OP_UNARY_INCREMENT,  // op1: name     op2:
+    OP_UNARY_INVERT,     
+    OP_UNARY_DECREMENT,  
+    OP_UNARY_INCREMENT,  
     OP_UNARY_NEGATION,
-    OP_RETURN,  // op1: rel_idx  op2:
+    OP_STORE,      
+    OP_POP,        
+    OP_CALL,
+    OP_RETURN,
     OP_LOAD_NULL,
     OP_LOAD_LITERAL,
-    OP_CALL,  // op1: name     op2: _arg1 count
+    OP_LOAD_VALUE,
+    OP_LOAD_ATTR,
     OP_MAKE_CONST,
     OP_MAKE_VAR,
     OP_MAKE_GLOBAL,
     OP_MAKE_LOCAL,
     OP_MAKE_CLASS,
-    OP_MAKE_FUNC,       // op1: name     op2: instr. count
-    OP_MAKE_MODULE,     // op1: name     op2: instr. count
-    OP_CREATE_CONTEXT,  // op1: flag     op2:
+    OP_MAKE_FUNC,
+    OP_MAKE_MODULE,
+    OP_MAKE_ARG,
+    OP_MAKE_ALIAS,
+    OP_MAKE_ARRAY,
+    OP_MAKE_CLOSURE,
+    OP_CREATE_CONTEXT,
     OP_DESTROY_CONTEXT,
     OP_RESET_CONTEXT,
-    OP_DELETE,  // op1: name     op2:
-    OP_NOP,     // op1:          op2:
-    OP_JUMPF,   // op1: rel_idx  op2:
+    OP_DELETE,
+    OP_NOP,
+    OP_JUMPF,
     OP_JUMPB,
     OP_JUMPF_IF,
     OP_JUMPB_IF,
-    OP_EXIT,          // op1:      op2:
-    OP_INDEX_ACCESS,  // op1: name     op2: offset
-    OP_MAKE_ARG,      // op1: name       op2: type
-    OP_MAKE_ALIAS,
-    OP_MAKE_ARRAY,
-    OP_LOAD_VALUE,
+    OP_EXIT,
+    OP_INDEX_ACCESS,
     OP_CHECK_MEMBERSHIP,
-    OP_MAKE_CLOSURE,
 } RnOpCode;
 
 RnStringNative GetOpCodeAsString(RnOpCode opcode);
